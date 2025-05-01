c1ce277beafdbb2606a482901d87bdcec587998a
Autism2025 <rajalakshmynv0803@gmail.com>
2025-05-01 21:28:08 +0530

    app.py updated

--- models.py
+++ models.py
@@ -18,7 +18,7 @@ class Expense(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     amount = db.Column(db.Float, nullable=False)
     description = db.Column(db.String(255), nullable=False)
-    date = db.Column(db.String(50), nullable=False)
+    date = db.Column(db.Date, nullable=False)
     category = db.Column(db.String(50), nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
